e67f3e6a3b71cbb9def41d782fffebb2dfce8238
nabin3 <nabinchandramaiti6@gmail.com>
2024-10-04 22:47:50 +0530

    new URL, view and template to display lists

--- web_site/lists/views.py
+++ web_site/lists/views.py
@@ -4,7 +4,10 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    return render(request, 'home.xhtml')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.xhtml', {"items": items})
+    return render(request, 'list.xhtml', {'items': items})
